b4ee2eec14f54959cfec088fff7f82ac8c7f14cf
epifanio <epiesasha@me.com>
2014-04-23 23:57:19 -0400

    added magic function that has access to output metadata

--- pyecoop/lib/ecoop/epimagic.py
+++ pyecoop/lib/ecoop/epimagic.py
@@ -1,18 +1,15 @@
-# This code can be put in any Python module, it does not require IPython
-# itself to be running already.  It only creates the magics subclass but
-# doesn't instantiate it yet.
 from __future__ import print_function
-
 import json
 import os
 import io
-
 from IPython.core.magic import (Magics, magics_class, line_magic,
                                 cell_magic, line_cell_magic)
 from IPython.core import magic_arguments
 from IPython.core.display import display 
 from IPython.utils.py3compat import unicode_type
 from IPython.utils.path import unquote_filename
+
+from ecoop.userdict import ecoopuser
 
 class JustMetadata(object):
     def __init__(self, metadata):
@@ -37,31 +34,24 @@ class MyMagics(Magics):
     )
     
     @magic_arguments.argument(
-        'metadata', type=unicode_type,
-        help='metadata to write'
+        'username', type=unicode_type,
+        help='username to assign references to cell content'
     )
     
     @cell_magic
-    def writefile2(self, line, cell):
-        """Write the contents of the cell to a file.
-        """
-        args = magic_arguments.parse_argstring(self.writefile2, line)
-        print(args)
+    def writefileref(self, line, cell):
+
+        args = magic_arguments.parse_argstring(self.writefileref, line)
         filename = os.path.expanduser(unquote_filename(args.filename))
-        
         if os.path.exists(filename):
             print("Overwriting %s" % filename)
         else:
             print("Writing %s" % filename)
-        
         mode = 'a' if args.append else 'w'
         with io.open(filename, mode, encoding='utf-8') as f:
             f.write(cell)
-        
-
-        display(metadata={'filename': filename})
-        print(filename)
-
+        username = os.path.expanduser(args.username)
+        display('', metadata={'references': ecoopuser[username]})
 
 ip = get_ipython()
 ip.register_magics(MyMagics)
